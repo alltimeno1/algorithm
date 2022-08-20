7f2eccba66463b67ffb254e8651a21ae9e082003
SungJun Kim <98739079+alltimeno1@users.noreply.github.com>
2022-08-20 12:56:13 +0900

    8/20

--- baekjoon/prime_factorization.js
+++ baekjoon/prime_factorization.js
@@ -1,2 +1,23 @@
 const fs = require('fs')
-const input = fs.readFileSync('baekjoon/input.txt').toString()
+const input = +fs.readFileSync('baekjoon/input.txt').toString()
+const decimals = []
+let num = input
+
+for (let i = 2; i * i <= input; i++) {
+  if (num < i) {
+    break
+  }
+
+  while (num % i === 0) {
+    decimals.push(i)
+    num = num / i
+  }
+}
+
+if (num !== 1) {
+  decimals.push(num)
+}
+
+if (decimals.length) {
+  console.log(decimals.join('\n'))
+}
